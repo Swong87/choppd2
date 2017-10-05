b70b8370abf7f5117a192638fb5c2de968742962
Swong87 <8swordz@gmail.com>
2017-10-04 21:26:51 -0400

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const challengeRoutes = require("./challenges");
 
 // API Routes
 router.use("/api", apiRoutes);
+
+// Get Routes
+router.use("/discover", challengeRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
