5b95246ce3b39147f94005dfc717749c82710f05
Swong87 <8swordz@gmail.com>
2017-10-04 20:52:56 -0400

    initial commit

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
-const recipeRoutes = require("./recipes");
 const challengeRoutes = require("./challenges");
-const userRoutes = require("./users");
 
 // Book routes
 router.use("/recipes", challengeRoutes);
