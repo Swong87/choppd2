61709319a5e766ef9a52ab56b144a5caac1937e5
Swong87 <8swordz@gmail.com>
2017-10-06 12:19:58 -0400

    image upload

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const routes = require("./routes");
+import corsPrefetch from 'cors-prefetch-middleware';
+import imagesUpload from 'images-upload-middleware';
 // Initialize Express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serve up static assets
 app.use(express.static("client/build"));
+
+app.use('/static', express.static('./server/static'));
+app.use(corsPrefetch);
+app.post('/notmultiple', imagesUpload(
+    './server/static/files',
+    'http://localhost:9090/static/files'
+));
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
