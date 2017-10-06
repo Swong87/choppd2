a5c432a4e2b7a371d7ba05793ef96264e5029d77
Swong87 <8swordz@gmail.com>
2017-10-06 13:05:06 -0400

    image uploader

--- client/src/components/PicUploader/PicUploader.js
+++ client/src/components/PicUploader/PicUploader.js
@@ -1,20 +1,7 @@
 import React, { Component } from "react";
 import ImagesUploader from 'react-images-uploader';
 import 'react-images-uploader/styles.css';
-import express from 'express';
-import corsPrefetch from 'cors-prefetch-middleware';
-import imagesUpload from 'images-upload-middleware';
  
-const app = express();
-
-app.use('/static', express.static('./server/static'));
- 
-app.use(corsPrefetch);
- 
-app.post('/notmultiple', imagesUpload(
-    './server/static/files',
-    'http://localhost:9090/static/files'
-));
 
 class PicUploader extends Component {
     render() {
