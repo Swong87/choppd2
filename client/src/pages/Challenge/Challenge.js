68157434b0bf8653462921fbed794454363c9a0a
Swong87 <8swordz@gmail.com>
2017-10-06 18:29:37 -0400

    image uploader

--- client/src/pages/Challenge/Challenge.js
+++ client/src/pages/Challenge/Challenge.js
@@ -64,7 +64,11 @@ class Challenge extends Component {
               {this.state.challenge.title}
             </h1>
             <img width='500px' src={this.state.challenge.image} />
-            <span>{this.state.challenge.ingredients}</span>
+            <div>
+              {this.state.challenge.ingredients.map(item => 
+                <span>| {item} |</span>
+              )}
+            </div>
           </div>
           <form>
             <div className="form-group">
